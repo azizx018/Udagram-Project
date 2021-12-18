15f1761a92095aa92c39cb59fbb42a183b0dc24f
azizx018 <chiara.aziz@gmail.com>
2021-12-17 20:33:12 -0600

    Update sequelize.ts

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -3,7 +3,6 @@ import { config } from "./config/config";
 
 export const sequelize = new Sequelize({
   username: config.username,
-    
   password: config.password,
   database: config.database,
   host: config.host,
